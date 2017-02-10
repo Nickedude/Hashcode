f4467256cec8634cc4774df44e0432ba679c015c
Gustav Rödström <gustav.rodstrom@gmail.com>
2017-02-10 18:43:02 +0100

    pizza parser

--- Practice/pizza.hs
+++ Practice/pizza.hs
@@ -1,4 +1,5 @@
 import Data.Char
+import Data.Text.IO
 
 data Cell = Tomato | Mushroom
 
@@ -12,19 +13,34 @@ type C = Int
 data Slices = Slices [((R,C),(R,C))]
 
 test = readFile "pizza.txt"
-
+{-
 pizzaParser :: FilePath -> IO Pizza
 pizzaParser file = 
 	do 
-	text <- readFile file
-	let (row : _ : col: _ : mi : _ : ma : _ : contents) = text
-	let mi' = digitToInt mi
-   	let ma' = digitToInt ma
+	  text <- (readFile file :: String)
+	  (row : _ : col: _ : mi : _ : ma : _ : contents) <- (text :: String)
+	  mi' <- digitToInt mi
+   	ma' <- digitToInt ma
     let rows = digitToInt row
     let col' = digitToInt col
     let cells =  pizzaParser' contents [[]]
 	return (Pizza cells row' col' mi' ma')
-	  where
+	  whereo
     	pizzaParser' :: String -> [[Cell]] -> [[Cell]]
     	pizzaParser' ('\n':xs) ys = pizzaParser' xs (ys:[])
+-}
+
+
+readPizzaFile :: FilePath -> IO (String, String)
+readPizzaFile path =
+  do  (header : content) <- splitOn "\n" $ readFile path
+      (row, col, min', max') <- parseHeader parseHeader
+      cells <- map (map toCell) content
+      return Pizza cells row col min' max'
+      where
+        parseHeader :: String -> (Int, Int, Int, Int)
+        parseHeader ((r : _ : c: _ : mi : _ : ma : xs) = map (\s -> digitToInt s) (r:c:mi:ma)
+        toCell 'T' = Tomato
+        toCell 'M' = Mushroom
+
  
