48db54c27220bd9b0d517909d854319f77c0c8e5
Nickedude <niklas.erik.gustafson@gmail.com>
2017-02-23 18:37:09 +0100

    util

--- live/Util.java
+++ live/Util.java
@@ -1,30 +1,45 @@
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.io.*;
+
+
 public class Util {
    
 
-   public static Integer parseFile(Path filepath) {
+   public void parseFile(Path filepath) {
         try {
             List<String> lines = Files.lines(filepath, StandardCharsets.UTF_8).collect(Collectors.toList());
 
             String[] firstLine = lines.get(0).split("[\\s]");
-            rows = Integer.parseInt(firstLine[0]);
-            columns = Integer.parseInt(firstLine[1]);
-            maxSize = Integer.parseInt(firstLine[3]);
-            minCont = Integer.parseInt(firstLine[2]);
+            int rows = Integer.parseInt(firstLine[0]);
+            int columns = Integer.parseInt(firstLine[1]);
+            int maxSize = Integer.parseInt(firstLine[3]);
+            int minCont = Integer.parseInt(firstLine[2]);
 
             lines.remove(0);
 
-            cells = new Cell[rows][columns];
-            occupied = new int[rows][columns];
-
             for (int r = 0; r < rows; r++) {
-                for (int c = 0; c < columns; c++) {
-                    cells[r][c] = getType(lines.get(r).charAt(c));
-                    occupied[r][c] = -1; //Default value
-                }
+                
             }
 
         } catch (Exception e) {
             System.out.println("Couldn't read file: " + filepath);
         }
-    }	
+    }
+
+    public void printToFile (String path) {
+        try {
+            PrintWriter out = new PrintWriter(path);
+            out.println("Stuff");
+            out.println("More stuff");
+            out.close();
+        }
+        catch(FileNotFoundException e) {
+            System.out.println("File not found!");
+        }
+    }
 }
