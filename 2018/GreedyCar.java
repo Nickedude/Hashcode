0838e488fe2ef62c1e3e4f20f12f939fa3af3f76
Nickedude <niklas.erik.gustafson@gmail.com>
2018-03-01 21:05:41 +0100

    Greedy car alg

--- 2018/GreedyCar.java
+++ 2018/GreedyCar.java
@@ -16,11 +16,11 @@ public class GreedyCar {
 
         @Override
         public int compare(Ride o1, Ride o2) {
-            int timeDiff = o1.startTime - o2.startTime;
+            /*int timeDiff = o1.startTime - o2.startTime;
 
             if(timeDiff != 0) {
                 return timeDiff;
-            }
+            }*/
 
             int distDiff = o1.getDistance() - o2.getDistance();
             return distDiff;
@@ -31,10 +31,30 @@ public class GreedyCar {
 
     public World compute() {
         int rideCount = 0;
-        for(Car car : world.cars) {
-            if(rideCount < world.rides.size()) {
-                car.addRide(world.rides.get(rideCount++));
+
+        for(Ride ride : world.rides) {
+
+            // Select the car most fit for this ride
+            Car bestCar = null;
+            int bestTimeDiff = Integer.MAX_VALUE;
+
+            for(Car car : world.cars) {
+                int distance = car.getDistanceFrom(ride.startRow, ride.startColumn);    // Get the distance from the car to the start position
+                int timeDiff = ride.startTime - car.getTick();                          // Get the time it will take to travel to the start pos
+                int arrivalTick = car.getTick() + distance;                             // Calculate the arrival time
+
+                if((timeDiff < bestTimeDiff) && (arrivalTick <= ride.finnishTime)) {    // Select the closest car that will be there on time
+                    bestCar = car;
+                }
             }
+
+            if(bestCar == null) {
+                continue;
+            }
+
+            bestCar.addRide(ride);
+            bestCar.travelTo(ride.startRow, ride.startColumn);
+            bestCar.travelTo(ride.endRow, ride.endColumn);
         }
 
         return world;
