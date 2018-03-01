2d860debede5d520e1a9084db9c55a79c38af5c7
Nickedude <niklas.erik.gustafson@gmail.com>
2018-03-01 20:11:11 +0100

    Added getTick, travelTo, getDistanceFrom

--- 2018/Car.java
+++ 2018/Car.java
@@ -7,6 +7,7 @@ public class Car {
     List<Ride> rides = new ArrayList<>();
     private int r = 0;
     private int c = 0;
+    private int tick = 0;
 
     int getCurrentRow() {
         return r;
@@ -25,21 +26,50 @@ public class Car {
     }
 
     Ride getLastRide() {
+        if(rides.size() == 0) {
+            return null;
+        }
+
         return rides.get(rides.size() - 1);
     }
 
     int getEndTime() {
+        if(rides.size() == 0) {
+            return 0;
+        }
+
         return getLastRide().finnishTime;
     }
 
     int getEndColumn() {
+        if(rides.size() == 0) {
+            return 0;
+        }
+
         return getLastRide().endColumn;
     }
 
     int getEndRow() {
+        if(rides.size() == 0) {
+            return 0;
+        }
+
         return getLastRide().endRow;
     }
 
+    public int getDistanceFrom(int r, int c) {
+        return Math.abs(getCurrentRow() - r) + Math.abs(getCurrentColumn() - c);
+    }
+
+    public void travelTo(int r, int c) {
+        tick += getDistanceFrom(r, c);
+        this.r = r;
+        this.c = c;
+    }
+
+    public int getTick() {
+        return tick;
+    }
 
 
 }
